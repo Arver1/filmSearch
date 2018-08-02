ead43d3569005f7cd90e5b1c46ac1210db6040cf
Arver1 <aactepanov@mail.ru>
2018-08-02 07:19:03 +0300

    added a search

--- public/routes/search.js
+++ public/routes/search.js
@@ -1,10 +1,17 @@
 const path = require('path');
 const fs = require('fs');
-
+const omdb = require('../omdb');
+const url = require('url');
 
 function search(req, res){
+ const parsedUrl = url.parse(req.url, true);
+ const { title } = parsedUrl.query;
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+  omdb.get(title, (error, movie) => {
+    if(error) throw error;
+  });
   const stream = fs.createReadStream(path.resolve('movie.html'));
   stream.pipe(res);
 }
